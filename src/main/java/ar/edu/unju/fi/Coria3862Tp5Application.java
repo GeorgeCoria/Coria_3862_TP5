2bb22795d9cc5e85f119611eef69ebcab7888497
George <George@DESKTOP-Q9UGHMV>
2020-06-03 16:08:50 -0300

    Asunto: Inicializacion del proyecto tp5 Responsable: Coria Jorge tack00: Se creo el proyecto java con SpringBoot, para dar inicio al TP5 de Programacion visual

---
+++ src/main/java/ar/edu/unju/fi/Coria3862Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coria3862Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coria3862Tp5Application.class, args);
+	}
+
+}
