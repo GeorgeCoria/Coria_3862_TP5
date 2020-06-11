2e934ebef73b0bef90733fc68c45ffcb0d56f081
George <George@DESKTOP-Q9UGHMV>
2020-06-10 22:49:15 -0300

    Asunto: se agregan las html faltantes
    Responsable: Coria Jorge

--- src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -8,15 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import ar.edu.unju.fi.service.IUsuarioService;
 
+/**
+ * Clase designada por la annotation @Controller.
+ * Se la denomina de tipo controlador por que son las 
+ * encargadas de tomar peticiones y controlar que 
+ * respuesta debe ser presentada en la vista (pagina
+ * html).
+ * @author George
+ *
+ */
 @Controller
+@RequestMapping("/")
 public class MainController {
+	
 	
 	@Autowired
 	private IUsuarioService usuarioService;
 	
 	
-	@RequestMapping("/")
-	@GetMapping("Index")
+	@GetMapping("inicio")
 	public String main(Model model) {
 		model.addAttribute("usuario",usuarioService.mostrar().getNombre());
 		return "Index";
